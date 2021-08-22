04f148e4761c0d2c543baf8c6bce7f6a5acede24
Shachar Langbeheim <shachar@lightricks.com>
2021-08-22 15:54:15 +0300

    Add scrolling view.

---
+++ app/src/main/java/com/example/androidexercise1/ContactsAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.androidexercise1
+
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.os.Parcel
+import android.os.Parcelable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class Contact(val name:String, val phone:String, val imageURL: String, val email: String? = null )
+
+class ContactsAdapter(private val contacts: Array<Contact>) :
+    RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            textView = view.findViewById(R.id.textView)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.contacts_item, viewGroup, false)
+        val gradientDrawable = GradientDrawable(
+            GradientDrawable.Orientation.LEFT_RIGHT,
+            intArrayOf(
+                Color.parseColor("#008000"),
+                Color.parseColor("#ADFF2F"))
+        );
+        gradientDrawable.cornerRadius = 4f;
+
+        //Set Gradient
+        view.setBackground(gradientDrawable)
+        view.getLayoutParams().height = 100;
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your contacts at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView.text = contacts[position].name
+    }
+
+    // Return the size of your contacts (invoked by the layout manager)
+    override fun getItemCount() = contacts.size
+
+}
--- app/src/main/java/com/example/androidexercise1/MainActivity.kt
+++ app/src/main/java/com/example/androidexercise1/MainActivity.kt
@@ -3,18 +3,61 @@ package com.example.androidexercise1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidexercise1.R.layout
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val intent = Intent(this, ContactDetailActivity::class.java).apply {
-            putExtra("NAME", "G pink")
-            putExtra("PHONE", "0000")
-            putExtra("EMAIL", "g-pink@png")
-            putExtra("IMAGE", "gPink.png")
-        }
-        startActivity(intent)
+        setContentView(layout.activity_main)
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        val contacts = arrayOf(
+            Contact("4 gold", "1234", "4Gold.png"),
+            Contact("a teal", "2345", "aTeal.png"),
+            Contact("b gold", "3456", "bGold.png"),
+            Contact("d black", "4567", "dBlack.png"),
+            Contact("e brown", "5678", "eBrown.png"),
+            Contact("g pink", "6789", "gPink"),
+            Contact("h brown", "1234", "hBrown.png"),
+            Contact("i red", "7890", "iRed.png"),
+            Contact("n fire", "8901", "nFire.png"),
+            Contact("n gold", "9012", "nGold.png"),
+            Contact("n metal", "0123", "nMetal.png"),
+            Contact("p smile", "1234", "pSmile.png"),
+            Contact("r sea", "2345", "rSea.png"),
+            Contact("s pink", "3456", "sPink.png"),
+            Contact("s red", "4567", "sRed.png"),
+            Contact("u gold", "5678", "uGold.png"),
+            Contact("u metal", "6789", "uMetal.png"),
+            Contact("4 gold + email", "1234", "4Gold.png", "foo@bar"),
+            Contact("a teal + email", "2345", "aTeal.png", "foo@bar"),
+            Contact("b gold + email", "3456", "bGold.png", "foo@bar"),
+            Contact("d black + email", "4567", "dBlack.png", "foo@bar"),
+            Contact("e brown + email", "5678", "eBrown.png", "foo@bar"),
+            Contact("g pink + email", "6789", "gPink", "foo@bar"),
+            Contact("h brown + email", "1234", "hBrown.png", "foo@bar"),
+            Contact("i red + email", "7890", "iRed.png", "foo@bar"),
+            Contact("n fire + email", "8901", "nFire.png", "foo@bar"),
+            Contact("n gold + email", "9012", "nGold.png", "foo@bar"),
+            Contact("n metal + email", "0123", "nMetal.png", "foo@bar"),
+            Contact("p smile + email", "1234", "pSmile.png", "foo@bar"),
+            Contact("r sea + email", "2345", "rSea.png", "foo@bar"),
+            Contact("s pink + email", "3456", "sPink.png", "foo@bar"),
+            Contact("s red + email", "4567", "sRed.png", "foo@bar"),
+            Contact("u gold + email", "5678", "uGold.png", "foo@bar"),
+            Contact("u metal + email", "6789", "uMetal.png", "foo@bar"))
+        val adapter = ContactsAdapter(contacts)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+//        val intent = Intent(this, ContactDetailActivity::class.java).apply {
+//            putExtra("NAME", "G pink")
+//            putExtra("PHONE", "0000")
+//            putExtra("EMAIL", "g-pink@png")
+//            putExtra("IMAGE", "gPink.png")
+//        }
+//        startActivity(intent)
     }
 }
